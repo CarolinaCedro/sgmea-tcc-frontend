933f4c3afe95d80f762eab7f0edc4b66b2138c05
CarolinaCedro <yotechgirl@gmail.com>
2024-08-19 11:50:30 -0300

    ++

--- src/app/core/auth/pages/forgot-password/forgot-password.component.ts
+++ src/app/core/auth/pages/forgot-password/forgot-password.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import {Component, inject, OnInit} from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ButtonComponent} from "../../../../shared/components/button/button.component";
+import {HttpClient} from "@angular/common/http";
+import {MatSnackBar, MatSnackBarConfig} from "@angular/material/snack-bar";
 
 
 @Component({
@@ -9,10 +11,45 @@ import {ButtonComponent} from "../../../../shared/components/button/button.compo
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss'],
   standalone: true,
-  imports: [FormsModule, RouterLink, ButtonComponent],
+  imports: [FormsModule, RouterLink, ButtonComponent, ReactiveFormsModule],
 })
 export class ForgotPasswordComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  form: FormGroup;
+  private _snackBar = inject(MatSnackBar);
+
+
+  constructor(private http: HttpClient, private router: Router, builder: FormBuilder) {
+    this.form = builder.group({
+      email: ['']
+    })
+  }
+
+
+  recoveryPassword(email: string) {
+    console.log("email", email)
+    this.http.post('http://localhost:8083/api/sgmea/v1/users/forgot-password', {email: email})
+      .subscribe(
+        response => {
+          this.openSnackBar('Instruções de recuperação de senha enviadas para seu email.');
+          this.router.navigate(['/auth/sign-in']);
+        },
+        error => {
+          this.openSnackBar('Erro ao enviar instruções de recuperação de senha. Verifique o email e tente novamente.');
+        }
+      );
+  }
+
+  ngOnInit(): void {
+  }
+
+  openSnackBar(message: string) {
+    const config: MatSnackBarConfig = {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    };
+
+    this._snackBar.open(message, "Fechar", config);
+  }
 }
--- src/app/shared/components/sgmea-form/sgmea-form.component.ts
+++ src/app/shared/components/sgmea-form/sgmea-form.component.ts
@@ -1,10 +1,12 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from "@angular/material/input";
 import {FormController} from "../../../modules/utis/models/form-controller.interface";
+import {map, mergeMap, take} from "rxjs/operators";
+import {Subject} from "rxjs";
 
 @Component({
   selector: 'sgmea-form',
@@ -19,7 +21,7 @@ import {FormController} from "../../../modules/utis/models/form-controller.inter
   templateUrl: './sgmea-form.component.html',
   styleUrl: './sgmea-form.component.scss',
 })
-export class SgmeaFormComponent {
+export class SgmeaFormComponent implements OnInit{
 
   @Input()
   formTitle?: string = 'Seu título aqui';
@@ -31,12 +33,73 @@ export class SgmeaFormComponent {
   @Input()
   formController: FormController<any>;
 
+  private unsubscribes: Subject<void> = new Subject();
+
+
   @Input()
   btnSaveShow?: boolean = true;
   @Input()
   iconModule!: string;
+  containsDomain: boolean = false;
 
   @Input() cardWidth?: string = '360px !important';
 
+
+
+  ngOnInit() {
+    this.containsDomain = this.formController?.containsMetadata();
+
+    if (!this.formController?.viewOnly) {
+      this.formController.isNewRecord()
+        .pipe(
+          map(isNewRecord => {
+            // Se não for um novo registro, ou seja, uma atualização, desabilita o formulário.
+            if (!isNewRecord) {
+              this.formController.form.disable({ emitEvent: false });
+            }
+          }),
+          take(1)
+        ).subscribe();
+    }
+  }
+
+
+
+  // ngOnInit() {
+  //   this.containsDomain = this.formController?.containsMetadata();
+  //   if (!this.formController?.viewOnly) {
+  //     this.formController.isNewRecord()
+  //       .pipe(
+  //         mergeMap(value => {
+  //           if (!value) {
+  //             return this.authService.hasRole(this.roleUpdate)
+  //               .pipe(
+  //                 map(result => {
+  //                   if (!result) {
+  //                     this.formController.form.disable({emitEvent: false});
+  //                   }
+  //                 })
+  //               );
+  //           } else {
+  //             return this.authService.hasRole(this.roleCreate)
+  //               .pipe(
+  //                 map(result => {
+  //                   if (!result) {
+  //                     this.formController.form.disable({emitEvent: false});
+  //                   }
+  //                 })
+  //               );
+  //           }
+  //         }),
+  //         take(1)
+  //       ).subscribe(() => {
+  //     });
+  //   }
+  // }
+
+
+  ngOnDestroy(): void {
+    this.unsubscribes.next();
+  }
 
 }
