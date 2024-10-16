e9454fd005a53a75a9142dd69b838531d34643db
CarolinaCedro <yotechgirl@gmail.com>
2024-10-16 18:22:19 -0300

    -force params

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:8083',
-  apiUrl: "http://ec2-3-81-92-145.compute-1.amazonaws.com:8083"
+  apiUrl: 'http://localhost:8083',
+  // apiUrl: "http://ec2-3-81-92-145.compute-1.amazonaws.com:8083"
 };
