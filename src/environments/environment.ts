934fd69454b927ce17b9fe8b1cfaa6b836a22bdb
Carolina <yotechgirl@gmail.com>
2024-10-16 22:33:46 -0300

    https variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:8083',
-  apiUrl: "http://ec2-3-208-47-127.compute-1.amazonaws.com:8083"
+  apiUrl: "https://ec2-3-208-47-127.compute-1.amazonaws.com:443"
 };
