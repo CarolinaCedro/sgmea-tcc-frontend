08eae414771a41e1ed2cb0701e8b5540d2b72551
CarolinaCedro <yotechgirl@gmail.com>
2024-10-21 21:52:13 -0300

    ++

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8083',
+  production: true,
+  apiUrl: 'https://cedro.dev.br',
   // apiUrl: "https://ec2-3-208-47-127.compute-1.amazonaws.com"
 };
